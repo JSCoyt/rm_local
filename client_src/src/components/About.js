74635eb35f8e71de7bbc3f7ff4556d9fd79dece9
JSCoyt <jscoyt@gmail.com>
2020-01-17 16:39:28 -0600

    EntregaFinal FullStack React&Loopback

---
+++ client_src/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => (
+  <div>
+    <h1>Acerca De</h1>
+
+    <h3>Bienvenido a RescueMe!</h3>
+
+  </div>
+)
+
+export default About;
---
+++ client_src/src/components/AddMeetup.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import {Link} from 'react-router-dom';
+
+class AddMeetup extends Component{
+  addMeetup(newMeetup){
+    axios.request({
+      method:'post',
+      url:'http://localhost:3000/api/meetups',
+      data: newMeetup
+    }).then(response => {
+      this.props.history.push('/');
+    }).catch(err => console.log(err));
+  }
+
+  onSubmit(e){
+    const newMeetup = {
+      name: this.refs.name.value,
+      city: this.refs.city.value,
+      address: this.refs.address.value
+    }
+    this.addMeetup(newMeetup);
+    e.preventDefault();
+  }
+
+  render(){
+    return (
+     <div>
+        <br />
+       <Link className="btn grey" to="/">Atras</Link>
+       <h1>Agregar Usuario</h1>
+       <form onSubmit={this.onSubmit.bind(this)}>
+          <div className="input-field">
+            <input type="text" name="name" ref="name" />
+            <label htmlFor="name">Nombre</label>
+          </div>
+          <div className="input-field">
+            <input type="text" name="city" ref="city" />
+            <label htmlFor="city">Apellido</label>
+          </div>
+          <div className="input-field">
+            <input type="text" name="address" ref="address" />
+            <label htmlFor="address">Tipo</label>
+          </div>
+          <input type="submit" value="Guardar" className="btn" />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default AddMeetup;
